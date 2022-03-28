fe4f9f0be2315b9d53ab9a1b65d69a1aa4bbd104
zDeadNow123 <dinnuhgamer@gmail.com>
2022-03-28 11:37:01 -0300

    Primeiro Commit

---
+++ CATI9101/CATI9101/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace CATI9101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
