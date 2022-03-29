776a432e01467e3ef0d77507ff9f6001307072d6
zDeadNow123 <dinnuhgamer@gmail.com>
2022-03-29 11:26:37 -0300

    Terceiro Commit

--- CATI9101/CATI9101/Program.cs
+++ CATI9101/CATI9101/Program.cs
@@ -1,15 +1,63 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
 
 namespace CATI9101
+
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
+    internal class Program {
+        // Atributos
+
+        // Construtores
+
+        // Métodos de acesso (get e set) - C# - Propriedades
+
+        // Métodos de classe - função/operação/ação do objeto
+
+        static void Main(string[] args) {
+
+
+            
+            // Declaração de variáveis
+
+            /*
+            var x = 10;
+            var nome = "Maria";
+            var teste = true;
+            var valor = 12.89;
+            var num = 15f;
+            var y = false;
+            byte[] vetor = new byte[5]; // Matriz de bytes
+            vetor[0] = 87;
+            vetor[1] = 101;
+            vetor[2] = 108;
+            vetor[3] = 108;
+            vetor[4] = 115;
+
+            for (int i = 0; i < vetor.Length; i++) { 
+                
+                Console.Write(vetor[i]);
+            }
+
+            int w = 0;
+            byte l = 0;
+     
+            double A, B, C, D;
+            A = 2.55;
+            float c = 10.65f;
+
+
+            // Formas de conversões de tipos
+            */
+
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o ano de seu nascimento:");
+            //int ano = int.Parse(Console.ReadLine());
+            int ano = Convert.ToInt32(Console.ReadLine());
+            //byte idade = (byte)(DateTime.Now.Year - (byte)ano);
+            byte idade = Convert.ToByte(DateTime.Now.Year - ano);
+
+            Console.WriteLine("Olá {0}, sua idade é {1}", nome, idade);
+            Console.ReadKey();
+        }       
     }
 }
