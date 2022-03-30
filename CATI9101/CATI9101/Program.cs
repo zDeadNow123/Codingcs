6e081fba20517ede0797f1e9096d50aeb708eeb3
zDeadNow123 <dinnuhgamer@gmail.com>
2022-03-30 10:14:15 -0300

    Quinto Commit

--- CATI9101/CATI9101/Program.cs
+++ CATI9101/CATI9101/Program.cs
@@ -59,51 +59,61 @@ namespace CATI9101
             Console.WriteLine("Olá {0}, sua idade é {1}", nome, idade);
         */
 
-        bool atende = false;
-        int[] vnumero = new int[10];
-        string sair = string.Empty;
-        int cont = 10;
+            // Declaração de variáveis
 
-        while(sair != "s" && cont > 0) {
+            bool atende = false;
+            int[] vnumero = new int[10];
+            string sair = string.Empty;
+            int cont = 10;
 
-            Console.WriteLine("Digite um número de 1 a 10");
-            int numero = int.Parse(Console.ReadLine());
-            int resultado = 0;
-        
-            do {
 
+
+
+            while(sair != "s" && cont > 0) {
+
+                // Mensagem de entrada do usuário
+                Console.WriteLine("Digite um número de 1 a 10");
+
+                // Declaração de variáveis
+                int numero = int.Parse(Console.ReadLine());
+                int resultado = 0;
+            
+
+
+                // Verifica se o numero não é maior que 0 e se não é menor que 11
                 if (!(numero > 0 && numero < 11)) {
                     Console.WriteLine("Você deve digitar um número de 1 a 10");
-                    atende = false;
                     Console.ReadKey();
                     return;
                 }
 
+                // Percorre os números da tabuada escolhida de 1 a 10 e exibe o resultado
                 for (int i = 1; i <= 10; i++) {
                     resultado = numero * i;
                     Console.WriteLine("{0} x {1} = {2}", numero, i, resultado);
                 }
-                atende = true;
+
+                // Declaração de variáveis
+                cont--;
+                vnumero[cont] = numero;
+
+                // Definindo a saída
+                Console.WriteLine("Deseja sair? (s) ou enter para continuar:");
+                sair = Console.ReadLine();
+                Console.Clear();
+            }
+
+                // Exibindo a tabuada
+                Console.Write("Você exibiu as tabuadas dos números: ");
 
 
-            } while (!atende);
+                // Percorre o vetor vnumero e exibe os valores
+                foreach (var item in vnumero) {
+                    if (item != 0) Console.Write($"{item}, ");
+                }
 
-            cont--;
-            vnumero[cont] = numero;
-
-            Console.WriteLine("Deseja sair? (s) ou enter para continuar:");
-            sair = Console.ReadLine();
-            Console.Clear();
-        } // Fim do While
-
-        Console.Write("Você exibiu as tabuadas dos números: ");
-
-        foreach (var item in vnumero) {
-            if (item != 0) Console.Write($"{item}, ");
-        }
-
-        Console.ReadKey();
-        
-        } // Fim do Main       
-    } // Fim da classe Program
-} // Fim do namespace
+                // Aguarda a entrada do usuário
+                Console.ReadKey();
+        }  
+    }
+}
